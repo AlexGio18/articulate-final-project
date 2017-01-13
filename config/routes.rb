d747401c5138a8315b0543160aecd4144a1ba1af
Patrick Kolodgy <email": null>
2017-01-13 18:54:01 -0500

    Add test routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static#index"
 
-  post "/static" => "static#create"
+  get "/test", to: "static#test"
+  post "/static", to: "static#create"
 end
