b4e9eb0b6faf451618f24e3f4078ead478fc6b1a
Alex Giacobbe <email": null>
2017-01-14 16:54:26 -0500

    ajax call to users/:id/speech_results

--- app/assets/javascripts/components/web_speech.jsx
+++ app/assets/javascripts/components/web_speech.jsx
@@ -1,74 +1,74 @@
 class WebSpeech extends React.Component {
 
-  // handleClick(){
-  //       let finalTranscript = ''
-  //       let resultsContainer = document.getElementById('resultsContainer')
-  //       let altsContainer = document.getElementById('altsContainer')
-  //
-  //       var recognition = new webkitSpeechRecognition()
-  //       recognition.continuous = true
-  //       recognition.interimResults = true
-  //       recognition.lang = "en-US"
-  //       recognition.maxAlternatives = 1
-  //
-  //       recognition.onstart = function() {
-  //         console.log("recording")
-  //       }
-  //
-  //       recognition.onend = function() {
-  //         console.log("stopped recording")
-  //         let url = window.location.pathname
-  //
-  //         $.ajax({
-  //           url: url + "/speech_results",
-  //           method: "POST",
-  //           data: "text="+finalTranscript
-  //         }).done(function(response){
-  //           console.log(response)
-  //         })
-  //
-  //         $(resultsContainer).text(finalTranscript)
-  //       }
-  //
-  //       recognition.onresult = function(event) {
-  //         if (typeof(event.results) === 'undefined') {
-  //           recognition.stop()
-  //           console.log("something went wrong...")
-  //         }
-  //
-  //         let interimTranscript = ''
-  //         for (var i = event.resultIndex; i < event.results.length; i++) {
-  //           if (event.results[i].isFinal) {
-  //             finalTranscript += event.results[i][0].transcript
-  //             console.log(finalTranscript)
-  //           } else {
-  //             console.log("results so far: " + event.results[i][0].transcript)
-  //           }
-  //         }
-  //       }
-  //
-  //       let toggleButton = this.refs.stopPlayButton
-  //
-  //       if (toggleButton.textContent === "Start"){
-  //         recognition.start()
-  //         toggleButton.textContent = "Stop"
-  //       } else {
-  //         recognition.stop()
-  //         toggleButton.textContent = "Start"
-  //       }
-  //
-  //       // let startButton = document.getElementById('startRec')
-  //       // let stopButton = document.getElementById('stopRec')
-  //
-  //       // startButton.addEventListener('click', function(e) {
-  //       //   recognition.start()
-  //       // })
-  //       //
-  //       // stopButton.addEventListener('click', function(e) {
-  //       //   recognition.stop()
-  //       // })
-  //
-  // }
+  handleClick(){
+        let finalTranscript = ''
+        let resultsContainer = document.getElementById('resultsContainer')
+        let altsContainer = document.getElementById('altsContainer')
+
+        var recognition = new webkitSpeechRecognition()
+        recognition.continuous = true
+        recognition.interimResults = true
+        recognition.lang = "en-US"
+        recognition.maxAlternatives = 1
+
+        recognition.onstart = function() {
+          console.log("recording")
+        }
+
+        recognition.onend = function() {
+          console.log("stopped recording")
+          let url = window.location.pathname
+
+          $.ajax({
+            url: url + "/speech_results",
+            method: "POST",
+            data: "text="+finalTranscript
+          }).done(function(response){
+            console.log(response)
+          })
+
+          $(resultsContainer).text(finalTranscript)
+        }
+
+        recognition.onresult = function(event) {
+          if (typeof(event.results) === 'undefined') {
+            recognition.stop()
+            console.log("something went wrong...")
+          }
+
+          let interimTranscript = ''
+          for (var i = event.resultIndex; i < event.results.length; i++) {
+            if (event.results[i].isFinal) {
+              finalTranscript += event.results[i][0].transcript
+              console.log(finalTranscript)
+            } else {
+              console.log("results so far: " + event.results[i][0].transcript)
+            }
+          }
+        }
+
+        let toggleButton = this.refs.stopPlayButton
+
+        if (toggleButton.textContent === "Start"){
+          recognition.start()
+          toggleButton.textContent = "Stop"
+        } else {
+          recognition.stop()
+          toggleButton.textContent = "Start"
+        }
+
+        // let startButton = document.getElementById('startRec')
+        // let stopButton = document.getElementById('stopRec')
+
+        // startButton.addEventListener('click', function(e) {
+        //   recognition.start()
+        // })
+        //
+        // stopButton.addEventListener('click', function(e) {
+        //   recognition.stop()
+        // })
+
+  }
 
   componentDidMount() {
 
