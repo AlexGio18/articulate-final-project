79e6ab08de791bf774ef4c0394f64a0951c7ad42
Alex Giacobbe <email": null>
2017-01-13 21:16:47 -0500

    compenitize header and button page

---
+++ app/assets/javascripts/components/navbar.jsx
@@ -0,0 +1,28 @@
+class NavBar extends React.Component {
+
+  render() {
+    return(
+      <nav className="navbar navbar-inverse navbar-fixed-top white-opacity">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar" >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">Project name</a>
+          </div>
+          <div id="navbar" className="collapse navbar-collapse">
+            <ul className="nav navbar-nav">
+              <li><a href="/users/sign_in">Login</a></li>
+              <li><a href="/users/sign_up">Register</a></li>
+              <li><a href="/static">Home</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    )
+  }
+}
