65be7bcfe44550d8c48d32570d8fbdad8e553ee1
Alexandra Cooper <email": null>
2017-01-14 15:05:28 -0500

    Write method to call tone analyzer API and isolate desired responses in form of two variables. Each variable is an array. Each element of each array is a hash

--- test-api-calls.rb
+++ test-api-calls.rb
@@ -5,10 +5,9 @@
 
 require 'watson-api-client'
 
-# service = WatsonAPIClient::ToneAnalyzer.new(username: ENV["WATSON_TONE_USER_NAME"],
-#                                             password: ENV["WATSON_TONE_PASSWORD"],
-#                                             version: 'v3',
-#                                             version_date: '2016-05-19')
+service = WatsonAPIClient::ToneAnalyzer.new(username: ENV["WATSON_TONE_USER_NAME"],
+                                            password: ENV["WATSON_TONE_PASSWORD"],
+                                            version: '2016-05-19')
 
 service = WatsonAPIClient::AlchemyLanguage.new(apikey: ENV["WATSON_API_KEY"],
                                                verify_ssl: OpenSSL::SSL::VERIFY_NONE)
@@ -16,3 +15,14 @@ service = WatsonAPIClient::AlchemyLanguage.new(apikey: ENV["WATSON_API_KEY"],
 result = service.TextGetEmotion_get(text: "Hello i am some text", outputMode:"json")
 
 p JSON.parse(result.body)
+
+
+
+# FOR TONE ANALYZER
+
+def get_tone(username, password, text, version = '2016-05-19')
+  tone_endpoint = "https://#{username}:#{password}@gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=#{version}&text=#{text}"
+  response = JSON.parse(RestClient.get(tone_endpoint))
+  language_tone_array = response["document_tone"]["tone_categories"][1]["tones"]
+  social_tone_array = response["document_tone"]["tone_categories"][2]["tones"]
+end
