bf65f02c9c277acb5f6a1cb69ff1f503a25fe257
Patrick Kolodgy <email": null>
2017-01-14 19:47:46 -0500

    Update controller with results logic

--- app/controllers/speech_results_controller.rb
+++ app/controllers/speech_results_controller.rb
@@ -8,45 +8,89 @@ class SpeechResultsController < ApplicationController
   end
 
   def create
+    text = "did you know it's pretty interesting how every time somebody asks you to talk with nothing to say and you have what everybody wants you to shut up you have everything to say I could keep talking all day long and then people get upset and then as soon as they text me first story my mind goes blank"
+
     @user = User.find(params[:user_id])
-    @speech_result = SpeechResult.new
-    # get tone data
-    language_tone_array = tone_response["document_tone"]["tone_categories"][1]["tones"]
-    @doc_language_tone = DocLanguageTone.new
-    launguage_tone_array.map {|tone| DocLanguageTone.write_attribute(tone["tone_name"].downcase.to_sym, tone["score"])}
-
-    social_tone_array = response["document_tone"]["tone_categories"][2]["tones"]
-    binding.pry
-  end
-
-  def test
-    text = "so this is some test I'm speaking and generating text for the experimental program or trying to get speech to text in the browser then we're trying to get texts out of the browser and then we're trying to analyze the text using the Alchemy API very exciting stuff if you ask me but hey that's just my opinion"
     tone_response = get_tone(text)
-    @speech_result = SpeechResult.new(transcript: text)
+    @speech_result = SpeechResult.new(transcript: text, user: @user)
+    @speech_result.save
 
     # Parsing tone analyzer
 
     @doc_emotion = DocEmotion.new(speech_result: @speech_result)
     emotion_array = tone_response["document_tone"]["tone_categories"][0]["tones"]
-    parse_result(emotion_array, @doc_emotion)
+    parse_tone_result(emotion_array, @doc_emotion)
     @speech_result.doc_emotion = @doc_emotion
+    @doc_emotion.save
 
     @doc_language_tone = DocLanguageTone.new(speech_result: @speech_result)
     language_tone_array = tone_response["document_tone"]["tone_categories"][1]["tones"]
-    parse_result(language_tone_array, @doc_language_tone)
+    parse_tone_result(language_tone_array, @doc_language_tone)
     @speech_result.doc_language_tone = @doc_language_tone
+    @doc_language_tone.save
 
     @doc_social_tone = DocSocialTone.new(speech_result: @speech_result)
     social_tone_array = tone_response["document_tone"]["tone_categories"][2]["tones"]
-    parse_result(social_tone_array, @doc_social_tone)
+    parse_tone_result(social_tone_array, @doc_social_tone)
     @speech_result.doc_social_tone = @doc_social_tone
+    @doc_social_tone.save
 
     # Parsing Alchemy
 
     alchemy_response = get_alchemy_results(text)
 
+    alchemy_response["taxonomies"].map do |taxonomy|
+      Taxonomy.create(speech_result: @speech_result, confident: taxonomy["confident"], label: taxonomy["label"], score: taxonomy["score"])
+    end
+
+    alchemy_response["keywords"].map do |keyword|
+      Keyword.create(speech_result: @speech_result, relevance: keyword["relevance"], sentiment_score: keyword["sentiment"]["score"], sentiment_type: keyword["sentiment"]["type"], text: keyword["text"])
+    end
 
 
+
+  end
+
+  def test
+    text = "did you know it's pretty interesting how every time somebody asks you to talk with nothing to say and you have what everybody wants you to shut up you have everything to say I could keep talking all day long and then people get upset and then as soon as they text me first story my mind goes blank"
+
+    tone_response = get_tone(text)
+    @speech_result = SpeechResult.new(transcript: text)
+    @speech_result.save
+
+    # Parsing tone analyzer
+
+    @doc_emotion = DocEmotion.new(speech_result: @speech_result)
+    emotion_array = tone_response["document_tone"]["tone_categories"][0]["tones"]
+    parse_tone_result(emotion_array, @doc_emotion)
+    @speech_result.doc_emotion = @doc_emotion
+    @doc_emotion.save
+
+    @doc_language_tone = DocLanguageTone.new(speech_result: @speech_result)
+    language_tone_array = tone_response["document_tone"]["tone_categories"][1]["tones"]
+    parse_tone_result(language_tone_array, @doc_language_tone)
+    @speech_result.doc_language_tone = @doc_language_tone
+    @doc_language_tone.save
+
+    @doc_social_tone = DocSocialTone.new(speech_result: @speech_result)
+    social_tone_array = tone_response["document_tone"]["tone_categories"][2]["tones"]
+    parse_tone_result(social_tone_array, @doc_social_tone)
+    @speech_result.doc_social_tone = @doc_social_tone
+    @doc_social_tone.save
+
+    # Parsing Alchemy
+
+    alchemy_response = get_alchemy_results(text)
+
+    alchemy_response["taxonomies"].map do |taxonomy|
+      Taxonomy.create(speech_result: @speech_result, confident: taxonomy["confident"], label: taxonomy["label"], score: taxonomy["score"])
+    end
+
+    alchemy_response["keywords"].map do |keyword|
+      Keyword.create(speech_result: @speech_result, relevance: keyword["relevance"], sentiment_score: keyword["sentiment"]["score"], sentiment_type: keyword["sentiment"]["type"], text: keyword["text"])
+    end
+
+    binding.pry
   end
 
 end
