1e388a02065a26e6edebfa023c36f3e03a142a4b
Alex Giacobbe <email": null>
2017-01-15 11:08:31 -0500

    Add componentize for history page

---
+++ app/assets/javascripts/components/user_history_page.jsx
@@ -0,0 +1,23 @@
+class History extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      history = []
+    }
+  }
+
+  componentDidMount() {
+    let url = window.location.pathname
+    $.ajax({
+      url:
+    })
+  }
+
+  render(){
+
+    return (
+
+      <h1>Hey There</h1>
+    )
+  }
+}
