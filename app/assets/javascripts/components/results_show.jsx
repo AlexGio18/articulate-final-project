f7e110bcede4cc623ca35c631d87da46ef3c8c14
Patrick Kolodgy <email": null>
2017-01-15 17:07:31 -0500

    Move data call to results_show component

--- app/assets/javascripts/components/results_show.jsx
+++ app/assets/javascripts/components/results_show.jsx
@@ -1,9 +1,34 @@
 class ResultsShow extends React.Component{
+  constructor() {
+    super()
+    this.state = {
+      result_data: {}
+    }
+    this.getResult = this.getResult.bind(this)
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: '/json_test'
+    })
+    .done(this.getResult)
+  }
+
+  getResult(response) {
+    this.setState({
+      result_data: response
+    })
+    debugger
+  }
+
   render(){
     return(
   <div>
     <div id="chart">
       <BubbleChart/>
+    </div>
+    <div id="keywords">
+      <Keywords />
     </div>
     <div id="speech-container">
       <div id="speech">
