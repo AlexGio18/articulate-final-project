bdedadaddb381fc7758fb8a343efa7e481420f86
Mark Tanca <email": null>
2017-01-15 15:19:14 -0500

    user can press a button to display dropdown text version of speech

--- app/assets/javascripts/components/display_speech_text.jsx
+++ app/assets/javascripts/components/display_speech_text.jsx
@@ -1,11 +1,22 @@
 class SpeechText extends React.Component{
+
+  handleSpeechDisplay(){
+    if ($("#speech-text").is(":hidden")) {
+        $("#speech-text").slideDown(500);
+    }
+    else {
+        $("#speech-text").hide();
+    }
+  }
   constructor(){
     super()
     this.state = {
       speech: "",
     }
+    this.handleSpeechDisplay = this.handleSpeechDisplay.bind(this)
   }
   componentDidMount(){
+    $("#speech-text").hide()
     var that = this
     $.ajax({
       url: '/users/1/speech_results/1',
@@ -15,10 +26,15 @@ class SpeechText extends React.Component{
         speech: response.text.text
       })
     })
+
   }
   render(){
     return(
-      <p>{this.state.speech}</p>
+        <div className="dropdown">
+          <button className="btn btn-primary" type="button" data-toggle="dropdown" onClick={this.handleSpeechDisplay}>See Speech Text
+          <span className="caret"></span></button>
+          <p id ="speech-text">{this.state.speech}</p>
+      </div>
     )
   }
 }
