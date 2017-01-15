cbff1a12f69e70f4422d99441fb56ef085e80d73
Mark Tanca <email": null>
2017-01-15 18:13:03 -0500

    users can taoggle charts

--- app/assets/javascripts/components/stat_bubble_chart.jsx
+++ app/assets/javascripts/components/stat_bubble_chart.jsx
@@ -1,4 +1,71 @@
 class BubbleChart extends React.Component {
+
+
+  handleChartDisplay (event) {
+    if($(event.target).context.innerHTML === " Social "){
+      var chart = new CanvasJS.Chart("chartContainer",
+        {
+       data: [
+       {
+         type: "bubble",
+         showInLegend: false,
+         toolTipContent: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span><br/><strong>Confidency: {score}</strong><br/>",
+         dataPoints: [
+          { x: 9, y: 5, z:this.state.social_tone["agreeableness"]*100, name: "Openness", score: this.state.social_tone["agreeableness"] },
+          { x: 11, y: 20, z:this.state.social_tone["conscientiousness"]*100, name: "Conscientiousness", score: this.state.social_tone["conscientiousness"] },
+          { x: 5, y: 4, z:this.state.social_tone["emotional_range"]*100, name: "Extraversion", score: this.state.social_tone["emotional_range"] },
+          { x: 14, y: 26, z:this.state.social_tone["extraversion"]*100, name: "Agreeableness", score: this.state.social_tone["extraversion"] },
+          { x: 20, y: 4, z:this.state.social_tone["openness"]*100, name: "Emotional Range", score: this.state.social_tone["openness"] }
+          ]
+        }
+        ]
+      });
+      chart.render();
+    }
+    else if ($(event.target).context.innerHTML === " Emotional ") {
+      var chart = new CanvasJS.Chart("chartContainer",
+        {
+       data: [
+       {
+         type: "bubble",
+         showInLegend: false,
+         toolTipContent: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span><br/><strong>Confidency: {score}</strong><br/>",
+         dataPoints: [
+           { x: 16, y: 25, z:this.state.emotion_tone["anger"]*100, name: "Anger", score: this.state.emotion_tone["anger"] },
+           { x: 19, y: 17, z:this.state.emotion_tone["disgust"]*100, name: "Disgust", score:this.state.emotion_tone["disgust"] },
+           { x: 15, y: 10, z:this.state.emotion_tone["fear"]*100, name: "Fear", score: this.state.emotion_tone["fear"] },
+           { x: 18, y: 6, z:this.state.emotion_tone["joy"]*100, name: "Joy", score: this.state.emotion_tone["joy"] },
+           { x: 7, y: 12, z:this.state.emotion_tone["sadness"]*100, name: "Sadness", score:this.state.emotion_tone["sadness"]}
+          ]
+        }
+        ]
+      });
+      chart.render();
+    }
+
+    else if ($(event.target).context.innerHTML === " Language "){
+      var chart = new CanvasJS.Chart("chartContainer",
+        {
+       data: [
+       {
+         type: "bubble",
+         showInLegend: false,
+         toolTipContent: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span><br/><strong>Confidency: {score}</strong><br/>",
+         dataPoints: [
+           { x: 10, y: 14, z:this.state.language_tone["analytical"]*100, name: "Analytical", score:this.state.language_tone["analytical"] },
+           { x: 5, y: 20, z:this.state.language_tone["confident"]*100, name: "Confident", score: this.state.language_tone["confident"] },
+           { x: 12, y: 10, z:this.state.language_tone["tentative"]*100, name: "Tentative", score: this.state.language_tone["tentative"]}
+          ]
+        }
+        ]
+      });
+      chart.render();
+    }
+  else if ($(event.target).context.innerHTML === " All ") {
+      this.createChartAll()
+    }
+  }
+
   constructor(){
     super();
     this.state = {
@@ -7,9 +74,10 @@ class BubbleChart extends React.Component {
         social_tone: [],
         data: [],
     }
-
+    this.handleChartDisplay = this.handleChartDisplay.bind(this)
     this.createChartAll = this.createChartAll.bind(this)
   }
+
   componentDidMount(){
     var that = this
     $.ajax({
@@ -78,6 +146,20 @@ class BubbleChart extends React.Component {
  }
 
   render(){
-    return(  <div id="chartContainer"></div>)
+    return(
+      <div id="charts-container">
+        <div>
+          <table id= "chart-selection" onClick={this.handleChartDisplay}>
+            <tbody>
+              <td id="show-all"> All </td>
+              <td id="emotion-chart"> Emotional </td>
+              <td id="language-chart"> Language </td>
+              <td id="social-chart"> Social </td>
+            </tbody>
+          </table>
+        </div>
+        <div id="chartContainer"></div>)
+      </div>
+    )
   }
 }
