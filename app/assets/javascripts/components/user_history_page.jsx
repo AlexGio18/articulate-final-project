38839bdeb1d81da2e96c0d59ed6c180efa248695
Alex Giacobbe <email": null>
2017-01-15 11:20:51 -0500

    add map of arra

--- app/assets/javascripts/components/user_history_page.jsx
+++ app/assets/javascripts/components/user_history_page.jsx
@@ -2,22 +2,34 @@ class History extends React.Component {
   constructor(){
     super();
     this.state = {
-      history = []
+      history: []
     }
   }
 
   componentDidMount() {
     let url = window.location.pathname
     $.ajax({
-      url:
+      url: url,
+      method: 'GET'
+    }).done(this.getHistory)
+  }
+
+  getHistory(response) {
+    this.setState({
+      history: response
     })
   }
 
   render(){
 
     return (
-
-      <h1>Hey There</h1>
+      <div className="container">
+        <ul>
+          {this.state.history.map((result, i) =>
+            <Result result={result} key={result.id} />
+          )}
+        </ul>
+      </div>
     )
   }
 }
