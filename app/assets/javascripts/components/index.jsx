f12b33b9c00b3cd673a27675aab1a412c0c4e452
Alex Giacobbe <agiacobbe18@gmail.com>
2017-01-15 19:57:18 -0500

    setup ajax request and link to show page, have visualizer on front page

--- app/assets/javascripts/components/index.jsx
+++ app/assets/javascripts/components/index.jsx
@@ -10,7 +10,9 @@ class Index extends React.Component {
           <div className="starter-template container-padding">
             <h1>Just Press Start.</h1>
             <WebSpeech currentUser={this.props.current_user} />
+            <AudioVisualizer />
             <TextForm currentUser={this.props.current_user} />
+
           </div>
 
         </div>
