367be5c10c2579f9954182edfa284f9f078f7368
Patrick Kolodgy <email": null>
2017-01-16 18:09:18 -0500

    Basic component styling

--- app/assets/javascripts/components/results_show.jsx
+++ app/assets/javascripts/components/results_show.jsx
@@ -21,17 +21,54 @@ class ResultsShow extends React.Component{
     })
   }
 
+  getTimer(duration) {
+    var seconds = duration / 1000
+    var minutes = parseInt( seconds / 60 )
+    seconds = Math.round(seconds % 60)
+    return minutes+':'+seconds
+  }
+
 
 
   render(){
     return(
       <div id="results-container">
+        <div className="row meta-results">
+          <div className="col-sm-4">
+            <div className="result-box-sm">
+              <h1>{Math.round(this.state.result_data.wpm)}</h1>
+              <span className="result-box-text">words per minute</span>
+            </div>
+          </div>
 
-          {this.state.result_data.keywords && <Keywords keywords={this.state.result_data.keywords} />}
+          <div className="col-sm-4">
+            <div className="result-box-sm">
+              <h1>{this.getTimer(this.state.result_data.duration)}</h1>
+              <span className="result-box-text">total speech duration</span>
+            </div>
+          </div>
 
-          {this.state.result_data.taxonomies && <Taxonomies taxonomies={this.state.result_data.taxonomies} />}
+          <div className="col-sm-4">
+            <div className="result-box-sm">
+              <h1>{Math.round(this.state.result_data.wpm)}</h1>
+              <span className="result-box-text">words per minute</span>
+            </div>
+          </div>
+        </div>
 
-          {this.state.result_data.filler_words && <FillerWords fillers={this.state.result_data.filler_words }/>}
+        <div className="row row-eq-height meta-results">
+          <div className="col-sm-8">
+            <div className="result-box-md">
+              {this.state.result_data.taxonomies && <Taxonomies taxonomies={this.state.result_data.taxonomies} />}
+            </div>
+          </div>
+
+          <div className="col-sm-4">
+            <div className="result-box-sm">
+              {this.state.result_data.filler_words && <FillerWords fillers={this.state.result_data.filler_words }/>}
+            </div>
+          </div>
+        </div>
 
         <div id="chart">
           <BubbleChart userID={this.props.current_user.id}/>
--- app/assets/javascripts/components/taxonomy.jsx
+++ app/assets/javascripts/components/taxonomy.jsx
@@ -2,9 +2,8 @@ class Taxonomy extends React.Component {
   render() {
     return(
       <div className="taxonomy">
-        <h4>label: {this.props.taxonomy.label}</h4>
+        <h4>{this.props.taxonomy.label}</h4>
         <p>score: {this.props.taxonomy.score}</p>
-        <p>confident: {this.props.taxonomy.confident}</p>
       </div>
     )
   }
