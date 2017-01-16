2d872d3a270662ab7c3ea5f79b8e549b343f13e0
Alex Giacobbe <agiacobbe18@gmail.com>
2017-01-16 14:09:52 -0500

    test remove css precompile for heroku build

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( custom.css )
+Rails.application.config.assets.version = '1.0',
+# Rails.application.config.assets.precompile += %w( custom.css )
 
 
 # Add additional assets to the asset load path
