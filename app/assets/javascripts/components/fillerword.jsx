79006879ec28dcefb894387483a3f2b16d7127cb
Patrick Kolodgy <email": null>
2017-01-16 16:21:12 -0500

    Fix classname

--- app/assets/javascripts/components/fillerword.jsx
+++ app/assets/javascripts/components/fillerword.jsx
@@ -3,7 +3,7 @@ class FillerWord extends React.Component {
 
   render() {
     return(
-      <div class="filler-word">
+      <div className="filler-word">
         <p>Word: {this.props.filler.word}, Count: {this.props.filler.count}</p>
       </div>
     )
--- app/assets/javascripts/components/fillerwords.jsx
+++ app/assets/javascripts/components/fillerwords.jsx
@@ -1,7 +1,7 @@
 class FillerWords extends React.Component {
   render() {
     return (
-      <div class="fillers-container">
+      <div className="fillers-container">
         <h3>Filler words</h3>
         {this.props.fillers.map( (filler, i) =>
           <FillerWord filler={filler} key={i} />)}
