66d00ce860af179f2c6d4d30a9cb96e5e2fcf381
Mark Tanca <email": null>
2017-01-16 16:35:38 -0500

    stuff

--- app/assets/javascripts/components/audio_visualizer.jsx
+++ app/assets/javascripts/components/audio_visualizer.jsx
@@ -213,7 +213,7 @@ class AudioVisualizer extends React.Component {
       //   canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
       // }
 
-    }
+    }uju
 
     // function voiceChange() {
     //
@@ -240,10 +240,10 @@ class AudioVisualizer extends React.Component {
 
     // event listeners to change visualize and voice settings
 
-    visualSelect.onchange = function() {
-      window.cancelAnimationFrame(drawVisual);
-      visualize();
-    }
+    // visualSelect.onchange = function() {
+    //   window.cancelAnimationFrame(drawVisual);
+    //   visualize();
+    // }
 
     // voiceSelect.onchange = function() {
     //   voiceChange();
