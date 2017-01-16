a4f665c875e7b40be61f4b1007eff94944b550c0
Mark Tanca <email": null>
2017-01-15 20:44:49 -0500

    added colors for bubbles based on catagory bubble belonged too

--- app/assets/javascripts/components/stat_bubble_chart.jsx
+++ app/assets/javascripts/components/stat_bubble_chart.jsx
@@ -17,11 +17,11 @@ class BubbleChart extends React.Component {
          showInLegend: false,
          toolTipContent: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span><br/><strong>Confidency: {score}</strong><br/>",
          dataPoints: [
-          { x: 9, y: 5, z:this.state.social_tone["agreeableness"]*100, name: "Openness", score: this.state.social_tone["agreeableness"] },
-          { x: 11, y: 20, z:this.state.social_tone["conscientiousness"]*100, name: "Conscientiousness", score: this.state.social_tone["conscientiousness"] },
-          { x: 5, y: 4, z:this.state.social_tone["emotional_range"]*100, name: "Extraversion", score: this.state.social_tone["emotional_range"] },
-          { x: 14, y: 26, z:this.state.social_tone["extraversion"]*100, name: "Agreeableness", score: this.state.social_tone["extraversion"] },
-          { x: 20, y: 4, z:this.state.social_tone["openness"]*100, name: "Emotional Range", score: this.state.social_tone["openness"] }
+           { x: 9, y: 5, z:this.state.social_tone["agreeableness"]*100, name: "Openness", color: "#5AA85A", score: this.state.social_tone["agreeableness"] },
+           { x: 11, y: 20, z:this.state.social_tone["conscientiousness"]*100, name: "Conscientiousness", color: "#5AA85A",score: this.state.social_tone["conscientiousness"] },
+           { x: 5, y: 4, z:this.state.social_tone["emotional_range"]*100, name: "Extraversion", color: "#5AA85A", score: this.state.social_tone["emotional_range"] },
+           { x: 14, y: 26, z:this.state.social_tone["extraversion"]*100, name: "Agreeableness", color: "#5AA85A", score: this.state.social_tone["extraversion"] },
+           { x: 20, y: 4, z:this.state.social_tone["openness"]*100, name: "Emotional Range", color: "#5AA85A", score: this.state.social_tone["openness"] }
           ]
         }
         ]
@@ -43,11 +43,11 @@ class BubbleChart extends React.Component {
          showInLegend: false,
          toolTipContent: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span><br/><strong>Confidency: {score}</strong><br/>",
          dataPoints: [
-           { x: 16, y: 25, z:this.state.emotion_tone["anger"]*100, name: "Anger", score: this.state.emotion_tone["anger"] },
-           { x: 19, y: 17, z:this.state.emotion_tone["disgust"]*100, name: "Disgust", score:this.state.emotion_tone["disgust"] },
-           { x: 15, y: 10, z:this.state.emotion_tone["fear"]*100, name: "Fear", score: this.state.emotion_tone["fear"] },
-           { x: 18, y: 6, z:this.state.emotion_tone["joy"]*100, name: "Joy", score: this.state.emotion_tone["joy"] },
-           { x: 7, y: 12, z:this.state.emotion_tone["sadness"]*100, name: "Sadness", score:this.state.emotion_tone["sadness"]}
+           { x: 16, y: 25, z:this.state.emotion_tone["anger"]*100, name: "Anger", color: "#E5233F", score: this.state.emotion_tone["anger"] },
+           { x: 19, y: 17, z:this.state.emotion_tone["disgust"]*100, name: "Disgust", color: "#E5233F", score:this.state.emotion_tone["disgust"] },
+           { x: 15, y: 10, z:this.state.emotion_tone["fear"]*100, name: "Fear", color: "#E5233F", score: this.state.emotion_tone["fear"] },
+           { x: 18, y: 6, z:this.state.emotion_tone["joy"]*100, name: "Joy", color: "#E5233F", score: this.state.emotion_tone["joy"] },
+           { x: 7, y: 12, z:this.state.emotion_tone["sadness"]*100, name: "Sadness", color: "#E5233F", score:this.state.emotion_tone["sadness"] }
           ]
         }
         ]
@@ -63,16 +63,16 @@ class BubbleChart extends React.Component {
   		},
   		axisX: {
   			valueFormatString: " ",
-  		},    
+  		},
        data: [
        {
          type: "bubble",
          showInLegend: false,
          toolTipContent: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span><br/><strong>Confidency: {score}</strong><br/>",
          dataPoints: [
-           { x: 10, y: 14, z:this.state.language_tone["analytical"]*100, name: "Analytical", score:this.state.language_tone["analytical"] },
-           { x: 5, y: 20, z:this.state.language_tone["confident"]*100, name: "Confident", score: this.state.language_tone["confident"] },
-           { x: 12, y: 10, z:this.state.language_tone["tentative"]*100, name: "Tentative", score: this.state.language_tone["tentative"]}
+           { x: 10, y: 14, z:this.state.language_tone["analytical"]*100, name: "Analytical", color: "#3189AF", score:this.state.language_tone["analytical"] },
+           { x: 5, y: 20, z:this.state.language_tone["confident"]*100, name: "Confident", color: "#3189AF", score: this.state.language_tone["confident"] },
+           { x: 12, y: 10, z:this.state.language_tone["tentative"]*100, name: "Tentative", color: "#3189AF", score: this.state.language_tone["tentative"]}
           ]
         }
         ]
@@ -127,7 +127,7 @@ class BubbleChart extends React.Component {
     //   },
     //   axisX: {
     //    title:"Life Expectancy",
-    //    maximum: 30
+    //    minimim: 30
     //  },
     //  axisY: {
     //    title:"Fertility Rate"
@@ -146,21 +146,21 @@ class BubbleChart extends React.Component {
        toolTipContent: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span><br/><strong>Confidency: {score}</strong><br/>",
        dataPoints: [
 
-        { x: 16, y: 25, z:this.state.emotion_tone["anger"]*100, name: "Anger", score: this.state.emotion_tone["anger"] },
-        { x: 19, y: 17, z:this.state.emotion_tone["disgust"]*100, name: "Disgust", score:this.state.emotion_tone["disgust"] },
-        { x: 15, y: 10, z:this.state.emotion_tone["fear"]*100, name: "Fear", score: this.state.emotion_tone["fear"] },
-        { x: 18, y: 6, z:this.state.emotion_tone["joy"]*100, name: "Joy", score: this.state.emotion_tone["joy"] },
-        { x: 7, y: 12, z:this.state.emotion_tone["sadness"]*100, name: "Sadness", score:this.state.emotion_tone["sadness"]},
+        { x: 16, y: 25, z:this.state.emotion_tone["anger"]*100, name: "Anger", color: "#E5233F", score: this.state.emotion_tone["anger"] },
+        { x: 19, y: 17, z:this.state.emotion_tone["disgust"]*100, name: "Disgust", color: "#E5233F", score:this.state.emotion_tone["disgust"] },
+        { x: 15, y: 10, z:this.state.emotion_tone["fear"]*100, name: "Fear", color: "#E5233F", score: this.state.emotion_tone["fear"] },
+        { x: 18, y: 6, z:this.state.emotion_tone["joy"]*100, name: "Joy", color: "#E5233F", score: this.state.emotion_tone["joy"] },
+        { x: 7, y: 12, z:this.state.emotion_tone["sadness"]*100, name: "Sadness", color: "#E5233F", score:this.state.emotion_tone["sadness"]},
 
-        { x: 10, y: 14, z:this.state.language_tone["analytical"]*100, name: "Analytical", score:this.state.language_tone["analytical"] },
-        { x: 5, y: 20, z:this.state.language_tone["confident"]*100, name: "Confident", score: this.state.language_tone["confident"] },
-        { x: 12, y: 10, z:this.state.language_tone["tentative"]*100, name: "Tentative", score: this.state.language_tone["tentative"]},
+        { x: 10, y: 14, z:this.state.language_tone["analytical"]*100, name: "Analytical", color: "#3189AF", score:this.state.language_tone["analytical"] },
+        { x: 5, y: 20, z:this.state.language_tone["confident"]*100, name: "Confident", color: "#3189AF", score: this.state.language_tone["confident"] },
+        { x: 12, y: 10, z:this.state.language_tone["tentative"]*100, name: "Tentative", color: "#3189AF", score: this.state.language_tone["tentative"]},
 
-        { x: 9, y: 5, z:this.state.social_tone["agreeableness"]*100, name: "Openness", score: this.state.social_tone["agreeableness"] },
-        { x: 11, y: 20, z:this.state.social_tone["conscientiousness"]*100, name: "Conscientiousness", score: this.state.social_tone["conscientiousness"] },
-        { x: 5, y: 4, z:this.state.social_tone["emotional_range"]*100, name: "Extraversion", score: this.state.social_tone["emotional_range"] },
-        { x: 14, y: 26, z:this.state.social_tone["extraversion"]*100, name: "Agreeableness", score: this.state.social_tone["extraversion"] },
-        { x: 20, y: 4, z:this.state.social_tone["openness"]*100, name: "Emotional Range", score: this.state.social_tone["openness"] }
+        { x: 9, y: 5, z:this.state.social_tone["agreeableness"]*100, name: "Openness", color: "#5AA85A", score: this.state.social_tone["agreeableness"] },
+        { x: 11, y: 20, z:this.state.social_tone["conscientiousness"]*100, name: "Conscientiousness", color: "#5AA85A",score: this.state.social_tone["conscientiousness"] },
+        { x: 5, y: 4, z:this.state.social_tone["emotional_range"]*100, name: "Extraversion", color: "#5AA85A", score: this.state.social_tone["emotional_range"] },
+        { x: 14, y: 26, z:this.state.social_tone["extraversion"]*100, name: "Agreeableness", color: "#5AA85A", score: this.state.social_tone["extraversion"] },
+        { x: 20, y: 4, z:this.state.social_tone["openness"]*100, name: "Emotional Range", color: "#5AA85A", score: this.state.social_tone["openness"] }
         ]
       }
       ]
