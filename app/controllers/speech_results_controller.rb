8b893f81a648dd68fb013600823157fd51d42123
Patrick Kolodgy <email": null>
2017-01-16 11:41:13 -0500

    Clean up speech results actions

--- app/controllers/speech_results_controller.rb
+++ app/controllers/speech_results_controller.rb
@@ -1,4 +1,3 @@
-
 class SpeechResultsController < ApplicationController
   before_action :authenticate_user!, only: [:show, :index]
 
@@ -12,7 +11,6 @@ class SpeechResultsController < ApplicationController
     else
       render json: { errors: "Forbidden" }, status: 403
     end
-
   end
 
   def test
@@ -20,6 +18,8 @@ class SpeechResultsController < ApplicationController
       {"id":6,
         "created_at":"2017-01-15T19:57:32.569Z",
         "updated_at":"2017-01-15T19:57:32.569Z",
+        "duration": 30000,
+        "wpm": 75,
         "transcript":"okay is this working now it is her that's nice but like why is it isn't working no interesting interesting empty quotes they all begin with auntie quotes no nevermind",
         "user":{"id":8,"first_name":"patrick","last_name":"kolo"},
         "taxonomies":[{"label":"/art and entertainment/movies and tv/movies","confident":"no","score":0.333912},{"label":"/art and entertainment/music","confident":"no","score":0.313142},{"label":"/hobbies and interests/reading","confident":"no","score":0.249649}],
@@ -33,106 +33,9 @@ class SpeechResultsController < ApplicationController
   end
 
   def show
-
-    speech = {
-      "text": {"text": "I am happy to join with you today in what will go down in history as the greatest demonstration for freedom in the history of our nation.
-
-Five score years ago, a great American, in whose symbolic shadow we stand today, signed the Emancipation Proclamation. This momentous decree came as a great beacon light of hope to millions of Negro slaves who had been seared in the flames of withering injustice. It came as a joyous daybreak to end the long night of their captivity."},
-      "document_tone": {
-        "tone_categories": [
-          {
-            "tones": [
-              {
-                "score": 0.122074,
-                "tone_id": "anger",
-                "tone_name": "Anger"
-              },
-              {
-                "score": 0.466388,
-                "tone_id": "disgust",
-                "tone_name": "Disgust"
-              },
-              {
-                "score": 0.162443,
-                "tone_id": "fear",
-                "tone_name": "Fear"
-              },
-              {
-                "score": 0.542211,
-                "tone_id": "joy",
-                "tone_name": "Joy"
-              },
-              {
-                "score": 0.225703,
-                "tone_id": "sadness",
-                "tone_name": "Sadness"
-              }
-            ],
-            "category_id": "emotion_tone",
-            "category_name": "Emotion Tone"
-          },
-          {
-            "tones": [
-              {
-                "score": 0.867915,
-                "tone_id": "analytical",
-                "tone_name": "Analytical"
-              },
-              {
-                "score": 0,
-                "tone_id": "confident",
-                "tone_name": "Confident"
-              },
-              {
-                "score": 0.664434,
-                "tone_id": "tentative",
-                "tone_name": "Tentative"
-              }
-            ],
-            "category_id": "language_tone",
-            "category_name": "Language Tone"
-          },
-          {
-            "tones": [
-              {
-                "score": 0.694488,
-                "tone_id": "openness_big5",
-                "tone_name": "Openness"
-              },
-              {
-                "score": 0.68608,
-                "tone_id": "conscientiousness_big5",
-                "tone_name": "Conscientiousness"
-              },
-              {
-                "score": 0.500131,
-                "tone_id": "extraversion_big5",
-                "tone_name": "Extraversion"
-              },
-              {
-                "score": 0.287042,
-                "tone_id": "agreeableness_big5",
-                "tone_name": "Agreeableness"
-              },
-              {
-                "score": 0.315252,
-                "tone_id": "emotional_range_big5",
-                "tone_name": "Emotional Range"
-              }
-            ],
-            "category_id": "social_tone",
-            "category_name": "Social Tone"
-          }
-        ]
-      }
-    }
-
-    speech_result = SpeechResult.find(:id)
+    speech_result = SpeechResult.find(params[:id])
     if authorized?(speech_result.user)
-      respond_to do |format|
-        format.html #{ redirect_to 'index' }
-        format.json { render json: speech}
-      end
+        render json: speech_result
     else
       render json: { errors: "Forbidden" }, status: 403
     end
@@ -144,7 +47,6 @@ Five score years ago, a great American, in whose symbolic shadow we stand today,
     speech_result = SpeechResult.new(transcript: params[:text], user: user)
     speech_result.save
 
-    binding.pry
     # Parsing tone analyzer
 
     doc_emotion = DocEmotion.new(speech_result: speech_result)
