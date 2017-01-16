f71c3d619b2a36b8cc32f23b195746ec14ee0f0f
Patrick Kolodgy <email": null>
2017-01-16 11:00:09 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115194844) do
+ActiveRecord::Schema.define(version: 20170116155520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20170115194844) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "duration"
+    t.float    "wpm"
   end
 
   create_table "taxonomies", force: :cascade do |t|
