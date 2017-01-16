f32e15aa8d5e687bbc5592b1a4bce412c135464a
Patrick Kolodgy <email": null>
2017-01-16 10:00:51 -0500

    Previous changes

---
+++ app/assets/javascripts/components/keywords.jsx
@@ -0,0 +1,16 @@
+class Keywords extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div id="keywords-container">
+        <h3>Keywords:</h3>
+        {this.props.keywords.map( (keyword, i) =>
+          <Keyword keyword={keyword} key={i} />
+        )}
+      </div>
+    )
+  }
+}
