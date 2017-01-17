a9ba282c8e6c859f8e38dc1f4c575d8c6aac45ad
Patrick Kolodgy <email": null>
2017-01-17 15:11:16 -0500

    Methods for document analysis

--- app/assets/javascripts/components/evaluation.jsx
+++ app/assets/javascripts/components/evaluation.jsx
@@ -4,16 +4,27 @@ class Evaluation extends React.Component {
   }
 
   getStrongEmotion(emotion) {
-    debugger
     return this.props.data.doc_emotion[emotion] > .5
   }
 
+  getStrongSocialTone(tone) {
+    return this.props.data.doc_social_tone[tone] > .8
+  }
+
+  getStrongLanguageTone(tone) {
+    return this.props.data.doc_language_tone[tone] > .8
+  }
+
   render() {
-    var strongEmotions = Object.keys(this.props.data.doc_emotion).filter(this.getStrongEmotion.bind(this))
-    console.log(strongEmotions)
+    let strongEmotions = Object.keys(this.props.data.doc_emotion).filter(this.getStrongEmotion.bind(this))
+    let strongSocialTones = Object.keys(this.props.data.doc_social_tone).filter(this.getStrongSocialTone.bind(this))
+    let strongLanguageTones = Object.keys(this.props.data.doc_language_tone).filter(this.getStrongLanguageTone.bind(this))
+
     return (
       <div>
-        This speech ranks high in {strongEmotions.join(' ')}
+        Emotions: This speech ranks high in {strongEmotions.join(', ')}<br />
+        Social Tone: This speech ranks high in {strongSocialTones.join(', ')}<br />
+        Language Tone: This speech ranks high in {strongLanguageTones.join(', ')}
       </div>
     )
   }
