aa1d39d4bb2fec50d913bcadf8f33a7e31275944
Patrick Kolodgy <email": null>
2017-01-18 15:30:22 -0500

    Fix bug in avg wpm method

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,7 +59,7 @@ class User < ApplicationRecord
   end
 
   def average_user_wpm
-    find_average(valid_wpm_averages) / self.speech_length
+    find_average(valid_wpm_averages.pluck(:wpm)) / self.speech_length
   end
 
 end
