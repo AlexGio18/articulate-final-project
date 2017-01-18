0d9e5c89e8f92d74fcd86a595be7c84032422f46
Patrick Kolodgy <email": null>
2017-01-18 16:14:38 -0500

    Reorganize Textform

--- app/assets/javascripts/components/index.jsx
+++ app/assets/javascripts/components/index.jsx
@@ -13,22 +13,22 @@ webSpeechResults(data) {
     resultData: data,
   })
 }
-  componentDidMount(){
-  }
-
 
   render(){
     return(
       <div>
         <div className="starter-template container-padding">
           <div className="wrapper">
-          <WebSpeech currentUser={this.props.current_user} results={this.webSpeechResults}  />
-            <div className="container results" id="resultsContainer">
-            {this.state.resultData.transcript && <ResultsShow result_data={this.state.resultData} current_user={this.props.current_user}/>}</div>
-          </div>
-          <div id="textAnalyzer">
-            <h1>Text Analysis</h1>
-            <TextForm currentUser={this.props.current_user} />
+
+            <WebSpeech currentUser={this.props.current_user} results={this.webSpeechResults}  />
+
+            <div id="textAnalyzer">
+              <h1>Text Analysis</h1>
+              <TextForm currentUser={this.props.current_user} results={this.webSpeechResults} />
+            </div>
+
+            {this.state.resultData.transcript && <ResultsShow result_data={this.state.resultData} current_user={this.props.current_user}/>}
+
           </div>
         </div>
       </div>
