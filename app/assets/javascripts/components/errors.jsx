4adbbb59e836c279031221acf8d7032131f2ce6f
Alex Giacobbe <agiacobbe18@gmail.com>
2017-01-18 15:33:55 -0500

    fixed text

--- app/assets/javascripts/components/errors.jsx
+++ app/assets/javascripts/components/errors.jsx
@@ -2,7 +2,7 @@ class Errors extends React.Component{
   render(){
     return(
       <div className="alert alert-danger">
-        <strong>Hey You!</strong> Sorry but we didn't catch what you said. Make sure your microphone being allowed and try again.
+        <strong>Hey You!</strong> Sorry but we didn't catch what you said. Make sure your microphone is being allowed and try again.
       </div>
     )
   }
