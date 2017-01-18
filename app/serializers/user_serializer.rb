b6811b8b90dc63b72e04e7795b84b5e3d4c11702
Patrick Kolodgy <email": null>
2017-01-18 09:29:10 -0500

    Add user historical data to result

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name
+  attributes :id, :first_name, :last_name, :speech_length, :average_emotions, :average_social_tone, :average_language_tone, :average_user_wpm
 end
