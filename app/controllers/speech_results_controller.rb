fdca9cb357143e81d98c8ba7ac617e85a3a83c80
Patrick Kolodgy <email": null>
2017-01-19 10:19:13 -0500

    Refactor analysis out of controller

--- app/controllers/speech_results_controller.rb
+++ app/controllers/speech_results_controller.rb
@@ -26,26 +26,7 @@ class SpeechResultsController < ApplicationController
     speech_result.user = current_user
     speech_result.save
 
-    tone_response = get_tone(params[:speech_result][:transcript])
-
-    speech_result.get_doc_emotion(tone_response)
-    speech_result.get_doc_language_tone(tone_response)
-    speech_result.get_doc_social_tone(tone_response)
-
-    # Parsing Alchemy
-
-    alchemy_response = get_alchemy_results(params[:speech_result][:transcript])
-
-    alchemy_response["taxonomies"].map do |taxonomy|
-      Taxonomy.create(speech_result: speech_result, confident: taxonomy["confident"], label: taxonomy["label"], score: taxonomy["score"])
-    end
-
-    alchemy_response["keywords"].map do |keyword|
-      k_word = Keyword.create(speech_result: speech_result, relevance: keyword["relevance"], sentiment_score: keyword["sentiment"]["score"], sentiment_type: keyword["sentiment"]["type"], text: keyword["text"])
-      keyword_emotion = KeywordEmotion.create(anger: keyword["emotions"]["anger"], disgust: keyword["emotions"]["disgust"], fear: keyword["emotions"]["fear"], joy: keyword["emotions"]["joy"], sadness: keyword["emotions"]["sadness"])
-      k_word.keyword_emotion = keyword_emotion
-    end
-
+    speech_result.generate_analysis(speech_result.transcript)
 
     render json: speech_result
   end
